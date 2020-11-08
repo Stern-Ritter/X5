0250597a2dc487e854c0c488950b4302a8d98351
Kayen <fadeew.k@mail.ru>
2020-11-09 00:57:29 +0300

    homework 3 review

--- src/ru/x5/hw3/Task3/Aspirant.java
+++ src/ru/x5/hw3/Task3/Aspirant.java
@@ -1,7 +1,7 @@
 package ru.x5.hw3.Task3;
 
 public class Aspirant extends Student {
-    String scientificWork;
+    private String scientificWork;
     public Aspirant(String firstName, String lastName, String group, double averageMark, String scientificWork) {
         super(firstName, lastName, group, averageMark);
         this.scientificWork = scientificWork;
